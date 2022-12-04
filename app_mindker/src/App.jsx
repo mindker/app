184e63dcceb70517cd7d3e3bfaedbbeb95099e9b
mamupe <mario,muperez@gmail.com>
2022-12-04 13:06:09 +0100

    feat: instalacion inicial

---
+++ app_mindker/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import { useState } from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hola todo bien?</h1>
+    </div>
+  );
+};
+
+export default App;
