3616c6ac2bf07f27c99f350f543c95e2ae0ed736
mamupe <mario,muperez@gmail.com>
2022-12-11 12:51:13 +0100

    feat: detaildeck funcionando (inprogress)

--- app_mindker/src/pages/detailDeck/DetailDeck.jsx
+++ app_mindker/src/pages/detailDeck/DetailDeck.jsx
@@ -1,5 +1,15 @@
+import '../../components/Cards/DetailDeckCard';
+
+import React from 'react';
+
+import DetailDeckCard from '../../components/Cards/DetailDeckCard';
+
 const DetailDeck = () => {
-  return <div>DetailDeck</div>;
+  return (
+    <div>
+      <DetailDeckCard />
+    </div>
+  );
 };
 
 export default DetailDeck;
